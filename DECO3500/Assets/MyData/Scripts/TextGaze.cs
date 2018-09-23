f7f646e079f82b27631df9ab4d4b753e6e02008b
cutierobot <mikaelasomerville@gmail.com>
2018-09-23 17:57:36 +1000

    @cutierobot  changes the colour of the text when reticle gazes over it

--- DECO3500/Assets/MyData/Scripts/TextGaze.cs
+++ DECO3500/Assets/MyData/Scripts/TextGaze.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TextGaze : MonoBehaviour {
 
+	public TextMesh text;
+
+	/*Changes the console text colour when hovered over by reticle*/
 	public void OnGazeEnter() {
-		Debug.Log("looking at text");
+		text.color = new Color(130.0f/255.0f, 130.0f/255.0f, 130.0f/255.0f, 1f);
 	}
 
+	/*Changes the console text colour when hovered over by reticle*/
 	public void OnGazeLeave(){
-		Debug.Log("not looking at text");
+		text.color = new Color(255.0f/255.0f, 255.0f/255.0f, 255.0f/255.0f, 1f);
 	}
 }
+
