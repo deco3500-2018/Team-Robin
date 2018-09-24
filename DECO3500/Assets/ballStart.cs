629157526222c1a278b28bd9194ba42741ca1427
Jason Lee <singjiang.lee@uqconnect.edu.au>
2018-09-24 14:43:45 +1000

    Add ball move funcrion

---
+++ DECO3500/Assets/ballStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballStart : MonoBehaviour {
+
+    public GameObject Ball;
+    public bool ballMove;
+
+	// Update is called once per frame
+	void Update () {
+		if (ballMove == true)
+        {
+            Ball.transform.Translate(Vector3.down * Time.deltaTime * 5);
+
+        }
+
+        if (Ball.transform.position.y > 7f)
+        {
+            ballMove = false;
+        }
+	}
+    void OnMouseDown()
+    {
+        ballMove = true;
+    }
+}
